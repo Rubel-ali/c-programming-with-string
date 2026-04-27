f8bdecd8c37d683eeeb085ec3c7b9e73fcd00a0b
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2026-04-27 12:39:00 +0600

    count problem solve

--- B_Let_s_use_Getline.c
+++ B_Let_s_use_Getline.c
@@ -1,3 +1,15 @@
+// Given a string S. Print the string S from the beginning to the first '\' character without printing the '\'.
+
+// Hint: use function getline(cin, s).
+
+// Input
+// Only one line contains a string S (1 ≤ |S| ≤ 106) where |S| is the length of the string.
+
+// It's guaranteed that S will contain '\' symbol.
+
+// Output
+// Print the answer required above.
+
 #include <stdio.h>
 int main()
 {

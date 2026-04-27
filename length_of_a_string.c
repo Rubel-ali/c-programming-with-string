d791a75ee499af182a79835ce8d134b7dcfc67d9
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2026-04-27 11:54:55 +0600

    b lets use getline problem solve

--- length_of_a_string.c
+++ length_of_a_string.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include<string.h>
 int main()
 {
     char s[101];
     scanf("%s", s);
-    int count = 0;
-    for (int i = 0; s[i] != '\0'; i++)
-    {
-        count++;
-    }
-    printf("%d", count);
+    // int count = 0;
+    // for (int i = 0; s[i] != '\0'; i++)
+    // {
+    //     count++;
+    // }
+
+    int size = strlen(s);
+    printf("%d", size);
     return 0;
 }
